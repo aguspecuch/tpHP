c36fbd08423e0309c95010418b450c27c7e5ff93
soydaniram <danialeram18@gmail.com>
2021-04-13 15:47:33 -0300

    Cambio de la clase poder

--- src/app/Transporte/Transporte.java
+++ src/app/Transporte/Transporte.java
@@ -2,4 +2,9 @@ package app.Transporte;
 
 public class Transporte {
     
+    protected String nombreDelVehiculo;
+    protected String modeloDelVehiculo;
+    protected int capacidadDelVehiculo;
+    protected int velocidadDelVehiculo; 
+    
 }
