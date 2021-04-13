c26d59ca7608c8232d037af80dbca0685c42b48a
Anamarnoriega07 <anamarnoriega@gmail.com>
2021-04-13 18:59:04 -0300

    Se cargan los atributos de la subclase personaje y elfo

--- src/app/personajes/Elfo.java
+++ src/app/personajes/Elfo.java
@@ -1,6 +1,29 @@
 package app.personajes;
 
+import app.artefactos.Artefacto;
+
 public class Elfo extends Criatura {
+    
+    private int energiaMagica;
+    private Artefacto artefacto;
+
+        
+    public int getEnergiaMagica() {
+        return this.energiaMagica;
+    }
+    
+    public void setEnergiaMagica(int energiaMagica) {
+        this.energiaMagica = energiaMagica; 
+    }   
+
+    public Artefacto getArtefacto() {
+        return this.artefacto;
+    }
+    
+    public void setArtefacto(Artefacto artefacto) {
+        this.artefacto = artefacto; 
+    } 
+
     
 }
 
--- src/app/personajes/Personaje.java
+++ src/app/personajes/Personaje.java
@@ -1,5 +1,34 @@
 package app.personajes;
 
-public class Personaje {
+ public abstract class Personaje {
+
+    private String nombre;
+    private int salud;
+    private int edad;
+        
+    public String getNombre() {
+        return this.nombre;
+    }
     
+    public void setNombre(String nombre) {
+        this.nombre = nombre; 
+    }   
+
+    public int getSalud() {
+        return this.salud;
+    }
+    
+    public void setSalud(int salud) {
+        this.salud = salud; 
+    } 
+
+    public int getEdad() {
+        return this.edad;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad = edad; 
+    }   
+ 
+
 }
