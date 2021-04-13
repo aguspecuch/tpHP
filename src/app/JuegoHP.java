073e098f94cc2f094a7f2cbc05794e06f2e4897d
abrilmpalacin <abrilmpalacin@gmail.com>
2021-04-13 19:21:29 -0300

    A esta clase en realidad la creó Agus. Simplemente reordené unas cosas.

--- src/app/JuegoHP.java
+++ src/app/JuegoHP.java
@@ -2,17 +2,18 @@ package app;
 
 public class JuegoHP {
     
-    public JuegoHP(String nombreJuego){
-        this.setNombreJuego(nombreJuego);
-    }
-
     private String nombreJuego;
 
-    public String getNombreJuego(){
-        return this.nombreJuego;
+    public JuegoHP(String nombreJuego){ 
+        this.setNombreJuego(nombreJuego);
     }
 
     public void setNombreJuego(String nombreJuego){
         this.nombreJuego = nombreJuego;
     }
+
+    public String getNombreJuego(){
+        return this.nombreJuego;
+    }
+
 }
