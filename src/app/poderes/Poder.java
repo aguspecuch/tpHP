f18751972b4dd4bf250a17c9ca0c1e9e140236ce
abrilmpalacin <abrilmpalacin@gmail.com>
2021-04-13 18:36:26 -0300

    Creé la subclase Hechizo de Pla clase Poder y también las subclases de Hechizo y, además, creé un paquete por cada subclase de Hechizo.

--- src/app/poderes/Poder.java
+++ src/app/poderes/Poder.java
@@ -3,7 +3,7 @@ package app.poderes;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Poder {
+public static abstract class Poder {
 
     private String nombre;
     private String descripcion;
@@ -12,20 +12,20 @@ public abstract class Poder {
     public List<Poder> Poderes = new ArrayList<>(); // Creo la clase poder no tendria que tener una lista de poderes
                                                     // sino mejor la clase Perosnaje.
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }                   
+                                    
     public String getNombre() {
         return this.nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return this.descripcion;
-    }
-
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+    
+    public String getDescripcion() {
+        return this.descripcion;
     }
 
 }
