675c298b3f77ca09c9181daf9716c1a307081bb4
soydaniram <danialeram18@gmail.com>
2021-04-23 23:03:57 -0300

    add gitignore file v2

--- src/app/App.java
+++ src/app/App.java
@@ -2,11 +2,9 @@ package app;
 
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-
         JuegoHP nombreJuego = new JuegoHP("Duelo Magico");
-
         nombreJuego.iniciarJuego();
-
     }
 }
